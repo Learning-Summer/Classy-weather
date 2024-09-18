e186c084a14af91505274da4df58a9c77bc7b68f
unknown <bohdankoval3012@gmail.com>
2024-09-18 23:14:29 +0300

    application made

--- src/App-v1.js
+++ src/App-v1.js
@@ -58,16 +58,18 @@ function formatDay(dateStr) {
 
 
 class App extends React.Component {
-    state = {
-        location: 'Kyiv',
-        isLoading: false,
-        displayLocation: "",
-        weather: {},
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            location: 'Kyiv',
+            isLoading: false,
+            displayLocation: "",
+            weather: {},
+        }
+        this.fetchWeather = this.fetchWeather.bind(this);
+    }
 
-
-    // async fetchWeather() {
-    fetchWeather = async () => {
+    async fetchWeather() {
         try {
             this.setState({
                 // ...this.state,
@@ -110,21 +112,13 @@ class App extends React.Component {
         
     }
 
-    setLocation = (e) => {
-        this.setState({
-            location: e.target.value
-        });
-    };
-
     render() {
         return <div className="app">
             <h1>Classy Weather</h1>
-            <Input location={this.state.location}
-            onChangeLocation = {
-                this.setLocation
-            }
-            />
-
+            <div>
+                <input type="text" placeholder="Search from location ..." 
+                value={this.state.location} onChange={e => this.setState({location: e.target.value})}/>
+            </div>
             <button onClick={this.fetchWeather}>Get weather</button>
 
             {this.state.isLoading && <p className="loader">Loading ...</p>}
@@ -135,23 +129,6 @@ class App extends React.Component {
 }
 
 export default App;
-
-class Input extends React.Component {
-    render() {
-        return (
-            <div>
-                <input type="text" placeholder="Search from location ..." 
-                    value = {
-                        this.props.location
-                    }
-                    onChange = {
-                        this.props.onChangeLocation
-                    }
-                    />
-        </div>
-        )
-    }
-}
 
 
 class Weather extends React.Component {
